a81ae296afc9a75457d6781a2bd83660960e09e5
Overdjosed <jose.ancizar.667@gmail.com>
2024-12-10 19:25:53 +0100

    Cambiando a produccion

--- web-vue/backend/app.py
+++ web-vue/backend/app.py
@@ -211,4 +211,5 @@ with app.app_context():
     load_experience_from_json()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Obtiene el puerto de la variable de entorno
+    app.run(host='0.0.0.0', port=port, debug=True)
